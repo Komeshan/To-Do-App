65eef858143309e832e52341a201b7b399740239
Komeshan <komeshan002@gmail.com>
2024-09-04 16:18:09 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -49,8 +49,8 @@ const Todo = () => {
     
 
   return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
-        
+    <div className='bg-white place-self-center w-10/12 max-w-md flex flex-col p-5 min-h-[450px] rounded-xl sm:w-11/12 sm:p-7 sm:min-h-[550px] lg:w-11/12'>
+
         {/* ------ Title ------ */}
 
         <div className='flex items-center mt-7 gap-2'>
